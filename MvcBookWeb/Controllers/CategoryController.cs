908065fa8fe2b20dc0b26204b9e802c047a41767
thanhduy1512 <duongduyy1512@gmail.com>
2023-08-02 21:56:35 +0700

    add index category controller and view

---
+++ MvcBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcBookWeb.Data;
+using MvcBookWeb.Models;
+
+namespace MvcBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList(); 
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ MvcBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcBookWeb.Models;
+
+namespace MvcBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
